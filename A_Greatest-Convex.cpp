debc6bd9be79a81d6e4e3b86a21e554f4b6286a2
Nasrullah <89463001+nasdevs@users.noreply.github.com>
2023-01-06 21:09:51 +0800

    Initial commit

---
+++ A_Greatest-Convex.cpp
@@ -0,0 +1,28 @@
+/*
+contest : https://codeforces.com/contest/1768
+problem : https://codeforces.com/contest/1768/problem/A 
+created : 01.06.2023
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	// FILE I/O
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	#endif
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+        int k;
+        cin >> k;
+
+        cout << k-1 << '\n';
+    }
+}
